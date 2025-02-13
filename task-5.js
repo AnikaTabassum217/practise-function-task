78c6e0a636dd19e9d3742dbdea86daa9b502c253
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-02-13 19:30:23 +0600

    Task-5 done

--- task-5.js
+++ task-5.js
@@ -1,3 +1,19 @@
 // Write a function called odd_even() which takes an integer value and tells whether
 //  this value is even or odd. If even return Even. If odd return Odd
 
+function odd_even(num){
+// console.log(num)
+if(num%2 != 0){
+    const oddOrEven='Odd'
+    return oddOrEven;
+}
+else{
+    const oddOrEven='Even';
+    return oddOrEven
+}
+}
+const number=1000000;
+const result=odd_even(number)
+console.log(result)
+
+
